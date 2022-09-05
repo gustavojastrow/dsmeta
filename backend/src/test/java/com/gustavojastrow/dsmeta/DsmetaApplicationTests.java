b28d50d16c60b5bc1be41891f944e5a619aac7c3
Gustavo Jastrow <jaastrow@hotmail.com>
2022-09-05 11:47:54 -0300

    oi

---
+++ backend/src/test/java/com/gustavojastrow/dsmeta/DsmetaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gustavojastrow.dsmeta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
